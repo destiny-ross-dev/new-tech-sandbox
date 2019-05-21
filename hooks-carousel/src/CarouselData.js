9f049ecc5fb03ec1b48060fecfea6aefcb9b5d42
Destiny Ross <destinyross.dev@gmail.com>
2019-05-20 23:38:17 -0400

    set up carousel with hooks

---
+++ hooks-carousel/src/CarouselData.js
@@ -0,0 +1,37 @@
+import img0 from "./profimg0.png";
+import img1 from "./profimg1.jpeg";
+import img2 from "./profimg2.jpg";
+import img3 from "./profimg3.png";
+import img4 from "./profimg4.jpg";
+
+export const data = [
+  {
+    userName: "Daenerys Targaryen",
+    profimg: img0,
+    userComment:
+      "When my dragons are grown, we will take back what was stolen from me and destroy those who wronged me! We will lay waste to armies and burn cities to the ground!"
+  },
+  {
+    userName: "Margaery Tyrell",
+    profimg: img1,
+    userComment:
+      "I know what a burden you bear. You should let me share the load. There must be some things I could do to help you. It would put to rest all this talk that you and I are rivals for the king."
+  },
+  {
+    userName: "Jon Snow",
+    profimg: img2,
+    userComment:
+      "My father taught me big men fall just as quick as little ones if you put a sword through their hearts."
+  },
+  {
+    userName: "Arya Stark",
+    profimg: img3,
+    userComment: "A girl has no name."
+  },
+  {
+    userName: "Euron Greyjoy",
+    profimg: img4,
+    userComment:
+      "They cheated at dice. Or maybe I cheated. Someone cheated. They weren't good fighters. You won't miss them."
+  }
+];
